61ba2cc3026918a5f4ec0d2fe9c8b8936c985c40
RiccardoZappa <100212223+RiccardoZappa@users.noreply.github.com>
2024-04-11 20:14:04 +0200

    added flipping images project

---
+++ GpuComputing_es1/GpuComputing_es1/utils/GpuTimer.cu
@@ -0,0 +1,30 @@
+#include "GpuTimer.cuh"
+
+using namespace utils;
+
+GpuTimer::GpuTimer()
+{
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+}
+
+GpuTimer::~GpuTimer()
+{
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+}
+
+void GpuTimer::Start() {
+	cudaEventRecord(start, 0);
+}
+
+void GpuTimer::Stop() {
+	cudaEventRecord(stop, 0);
+}
+
+float GpuTimer::Elapsed() {
+	float elapsed;
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&elapsed, start, stop);
+	return elapsed;
+}
