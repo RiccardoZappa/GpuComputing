3eb23dbf8304d23bac74ab5f42c06180b20d33b5
RiccardoZappa <rzappa00@gmail.com>
2024-05-09 20:51:12 +0200

    fixed convolution kernel

--- GpuComputing/ConvolutionalProduct/ConvolutionProd.cu
+++ GpuComputing/ConvolutionalProduct/ConvolutionProd.cu
@@ -7,7 +7,7 @@ constexpr auto FILTER_RADIUS = 2;
 using namespace utils;
 __constant__ float f [FILTER_RADIUS * 2 + 1][FILTER_RADIUS * 2 + 1];
 
-__global__ void convolution_2D_basic_kernel(float* N, float* F, float* P,const int r, int width, int height)
+__global__ void convolution_2D_basic_kernel(float* N, float* P,const int r, int width, int height)
 {
     int out_col = blockIdx.x * blockDim.x + threadIdx.x;
     int out_row = blockIdx.y * blockDim.y + threadIdx.y;
@@ -20,12 +20,13 @@ __global__ void convolution_2D_basic_kernel(float* N, float* F, float* P,const i
             int in_Col = out_col - r + f_Col;
             if (in_Row >= 0 && in_Row < height && in_Col >= 0 && in_Col < width)
             {
-                Pvalue += F[f_Row * 2*r + f_Col] * N[in_Row * width + in_Col];
+                Pvalue += f[f_Row][f_Col] * N[in_Row * width + in_Col];
             }
 	    }
     }
     P[out_row * width + out_col] = Pvalue;
 }
+
 int main()
 {
 	GpuTimer gpu_timer;
